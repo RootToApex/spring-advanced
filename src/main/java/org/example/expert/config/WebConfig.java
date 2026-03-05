d1055c4db4ccd00c288fd65a9e0af72582a1abde
ljh <leejunghyun1019@gmail.com>
2026-03-05 11:48:22 +0900

    lv2 불필요한 else 제거

--- src/main/java/org/example/expert/config/WebConfig.java
+++ src/main/java/org/example/expert/config/WebConfig.java
@@ -1,4 +1,17 @@
 package org.example.expert.config;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration // 이 클래스는 설정 파일이라는 것을 알려주는 어노테이션
+public class WebConfig implements WebMvcConfigurer { // MVC 설정으로 바꾸기
+
+    // SpringMVC 설정에 ArgumentResolver를 등록
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new AuthUserArgumentResolver());
+    }
 }
